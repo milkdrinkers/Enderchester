d89e5164aacdb464389fb5fa2375806d2bfb3d89
darksaid98 <darksaid9889@gmail.com>
2025-06-14 08:48:46 +0200

    ci: update workflows from upstream template
    - Also replaces the gradle plugin used for publishing artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 import java.time.Instant
+import com.vanniktech.maven.publish.JavaLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     `java-library`
 
-    alias(libs.plugins.maven.deployer)
+    alias(libs.plugins.publisher)
     alias(libs.plugins.shadow) // Shades and relocates dependencies, see https://gradleup.com/shadow/
     alias(libs.plugins.run.paper) // Built in test server using runServer and runMojangMappedServer tasks
     alias(libs.plugins.plugin.yml) // Automatic plugin.yml generation
@@ -153,58 +156,66 @@ bukkit { // Options: https://github.com/Minecrell/plugin-yml#bukkit
     }
 }
 
-deployer {
-    release {
-        version.set("${rootProject.version}")
-        description.set(rootProject.description.orEmpty())
-    }
-
-    projectInfo {
-        groupId = "io.github.milkdrinkers"
-        artifactId = "enderchester"
-        version = "${rootProject.version}"
-
-        name = rootProject.name
-        description = rootProject.description.orEmpty()
-        url = "https://github.com/milkdrinkers/Enderchester"
-
-        scm {
-            connection = "scm:git:git://github.com/milkdrinkers/Enderchester.git"
-            developerConnection = "scm:git:ssh://github.com:milkdrinkers/Enderchester.git"
-            url = "https://github.com/milkdrinkers/Enderchester"
-        }
-
-        license("GNU General Public License Version 3", "https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
-
-        developer({
-            name.set("darksaid98")
-            email.set("darksaid9889@gmail.com")
-            url.set("https://github.com/darksaid98")
-            organization.set("Milkdrinkers")
-        })
-    }
-
-    content {
-        component {
-            fromJava()
-        }
-    }
-
-    centralPortalSpec {
-        auth.user.set(secret("MAVEN_USERNAME"))
-        auth.password.set(secret("MAVEN_PASSWORD"))
-    }
-
-    signing {
-        key.set(secret("GPG_KEY"))
-        password.set(secret("GPG_PASSWORD"))
-    }
-}
-
 fun applyCustomVersion() {
     // Apply custom version arg or append snapshot version
     val ver = properties["altVer"]?.toString() ?: "${rootProject.version}-SNAPSHOT-${Instant.now().epochSecond}"
 
     // Strip prefixed "v" from version tag
     rootProject.version = (if (ver.first().equals('v', true)) ver.substring(1) else ver.uppercase()).uppercase()
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.milkdrinkers",
+        artifactId = "enderchester",
+        version = rootProject.version.toString().let { originalVersion ->
+            if (!originalVersion.contains("-SNAPSHOT"))
+                originalVersion
+            else
+                originalVersion.substringBeforeLast("-SNAPSHOT") + "-SNAPSHOT" // Force append just -SNAPSHOT if snapshot version
+        }
+    )
+
+    pom {
+        name.set(rootProject.name)
+        description.set(rootProject.description.orEmpty())
+        url.set("https://github.com/milkdrinkers/Enderchester")
+        inceptionYear.set("2025")
+
+        licenses {
+            license {
+                name.set("GNU General Public License Version 3")
+                url.set("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
+                distribution.set("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("darksaid98")
+                name.set("darksaid98")
+                url.set("https://github.com/darksaid98")
+                organization.set("Milkdrinkers")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/milkdrinkers/Enderchester")
+            connection.set("scm:git:git://github.com/milkdrinkers/Enderchester.git")
+            developerConnection.set("scm:git:ssh://github.com:milkdrinkers/Enderchester.git")
+        }
+    }
+
+    configure(JavaLibrary(
+        javadocJar = JavadocJar.None(), // We want to use our own javadoc jar
+    ))
+
+    // Publish to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+
+    // Sign all publications
+    signAllPublications()
+
+    // Skip signing for local tasks
+    tasks.withType<Sign>().configureEach { onlyIf { !gradle.taskGraph.allTasks.any { it is PublishToMavenLocal } } }
 }
